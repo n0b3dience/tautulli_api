bdc120cb4589108af1508ea49c1c5a1d193ebe8f
John Nettles <nettles4349@gmail.com>
2019-02-14 22:36:56 -0500

    Added separate config file and removed configparser part from main script

---
+++ config.py
@@ -0,0 +1,12 @@
+import configparser
+
+
+# ConfigParser Variables
+config = configparser.ConfigParser()
+config.read('settings_private.ini')
+# Settings File Variables
+HOST = config['USER_SETTINGS']['host']
+PORT = config['USER_SETTINGS']['port']
+API_KEY = config['USER_SETTINGS']['api_key']
+SCHEMA = config['USER_SETTINGS']['schema']
+PATH = config['USER_SETTINGS']['path']
--- tautulli.py
+++ tautulli.py
@@ -3,18 +3,7 @@ Tautulli base class
 """
 from payload import Payload
 from requester import Requester
-import configparser
-
-
-# ConfigParser Variables
-config = configparser.ConfigParser()
-config.read('settings_private.ini')
-# Settings File Variables
-HOST = config['USER_SETTINGS']['host']
-PORT = config['USER_SETTINGS']['port']
-API_KEY = config['USER_SETTINGS']['api_key']
-SCHEMA = config['USER_SETTINGS']['schema']
-PATH = config['USER_SETTINGS']['path']
+from config import HOST, PORT, API_KEY, SCHEMA, PATH
 
 
 class Tautulli:
