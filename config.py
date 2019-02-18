fe6d78070def9edf90005ac8c2902ac4fa048b24
John Nettles <nettles4349@gmail.com>
2019-02-17 21:08:24 -0500

    commit

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import configparser
 
 # ConfigParser Variables
 config = configparser.ConfigParser()
-config.read('settings.ini')
+config.read('settings_private.ini')
 # Settings File Variables
 HOST = config['USER_SETTINGS']['host']
 PORT = config['USER_SETTINGS']['port']
